ba1bb8eeb089bed1c0bb9791d8169005aa7b1c6a
Umesh Joshi <uj19343@gmail.com>
2025-06-14 20:31:24 +0545

    these are java leaning code filr

---
+++ collage/pratice/Qn3.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Qn3{
+    static public void main(String[] args){
+        for (int i = 0; i<= 4; i++){
+            System.out.println("Hello umesh");
+        }
+    }
+    }
