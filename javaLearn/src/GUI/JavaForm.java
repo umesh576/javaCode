3a77d365a3e6daf691dde1b6f4d54c4e06a3424e
Umesh Joshi <uj19343@gmail.com>
2025-07-24 07:57:31 +0545

    Java form method added

---
+++ javaLearn/src/GUI/JavaForm.java
@@ -0,0 +1,37 @@
+package GUI;
+
+import java.awt.Container;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class JavaForm {
+
+
+    public static void main(String[] args) {
+        
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(100,100,500,500);
+        frame.setTitle("This is frame.");
+
+        Container cont = frame.getContentPane();
+        cont.setLayout(null);
+
+        
+        JLabel label = new JLabel("This is label");
+
+        Font font = new Font("arial",Font.BOLD,25);
+        label.setFont(font);
+
+        label.setBounds(100,100,300,300);
+        // label.setText("This is label");
+
+        cont.add(label);
+
+
+        frame.setVisible(true);
+    }
+    
+}
--- javaLearn/src/interface/Interface.java
+++ javaLearn/src/interface/Interface.java
@@ -1,7 +1,21 @@
 
-public class Interface{
+ interface InterfaceClass {
+    
+    final int a =10;
+    void readbook();
 
+    
+}
+
+public class Interface implements InterfaceClass{
+    public void readbook(){
+        System.out.println("I am reading book");
+    }
     public static void main(String[] args){
+
+        Interface i = new Interface();
+
+        i.readbook();
         
     }
 }
